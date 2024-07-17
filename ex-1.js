87fb42c28263349ac82a123448a4feebb348a3e3
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-17 14:13:30 +0700

    feat: answer ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,35 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let index = 0; index < students.length; index++) {
+    if (students[index] === searchStudent) {
+      return index;
+    }
+  }
+  return -1;
 }
 
+let students1 = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+let searchStudent1 = "John";
+console.log(
+  `${searchStudent1} เท่ากับ index ที่`,
+  findStudentIndex(students1, searchStudent1)
+);
+
+let students2 = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"];
+let searchStudent2 = "Andrew";
+console.log(
+  `${searchStudent1} เท่ากับ index ที่`,
+  findStudentIndex(students2, searchStudent2)
+);
+
 // ตอบคำถามตรงนี้จ้า
+// O(n)
